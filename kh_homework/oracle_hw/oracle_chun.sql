2bc57abb86a94559afd4d40848f6e122844f7e86
NamYunji <dbs7wl7@gmail.com>
2021-01-28 15:32:27 +0900

    hw_0127

--- kh_homework/oracle_hw/oracle_chun.sql
+++ kh_homework/oracle_hw/oracle_chun.sql
@@ -31,8 +31,9 @@ SELECT * FROM tb_grade;
 
 -------------------------------------------------
 
+
 -------------------------------------------------
---hw_0126
+--hw_0126 [Basic SELECT]
 -------------------------------------------------
 
 --1. 춘 기술대학교의 학과 이름과 계열을 표시하시오.
@@ -147,3 +148,171 @@ select department_no, class_type, count(*)
 from tb_class
 group by department_no, class_type
 having count(class_no) >= 10 and class_type = '전공선택';  
+
+
+-------------------------------------------------
+--hw_0127 [Additional SELECT - 함수]
+-------------------------------------------------
+
+--1. 영어영문학과(학과코드 002) 학생들의 학번과 이름, 입학 년도를
+--입학 년도가 빠른 순으로 표시하는 SQL 문장을 작성하시오.
+--단, 헤더는 "학번", "이름", "입학년도" 가 표시되도록 한다.)
+
+select student_no 학번,
+            student_name 이름, 
+            to_char(entrance_date, 'yyyy-mm-dd') 입학년도
+from tb_student
+where department_no = 002
+order by 입학년도;
+
+
+--2. 춘 기술대학교의 교수 중 이름이 세 글자가 아닌 교수가 한 명 있다고 한다.
+--그 교수의 이름과 주민번호를 화면에 출력하는 SQL 문장을 작성해 보자. 
+--(* 이때 올바르게 작성한 SQL 문장의 결과 값이 예상과 다르게 나올 수 있다. 원인이 무엇일지 생각해볼 것)
+
+select professor_name, professor_ssn
+from tb_professor
+where professor_name not like '___';
+
+
+--3. 춘 기술대학교의 남자 교수들의 이름과 나이를 출력하는 SQL 문장을 작성하시오.
+--단 이때 나이가 적은 사람에서 많은 사람 순서로 화면에 출력되도록 만드시오.
+--(단, 교수 중 2000년 이후 출생자는 없으며 출력 헤더는 "교수이름", "나이"로 한다. 나이는 ‘만’으로 계산한다.)
+
+--방법1.
+select professor_name 교수이름, 
+case
+--현재월 > 출생월    -> 현재년도 - 출생년도
+when extract(month from sysdate) > substr(professor_ssn, 3, 2)
+then extract(year from sysdate) - (1900+substr(professor_ssn, 1, 2))
+--현재월 = 출생월 & 현재날짜 >= 출생날짜    -> 현재년도 - 출생년도
+when extract(month from sysdate) = substr(professor_ssn, 3, 2)
+        and extract(day from sysdate) >= substr(professor_ssn, 5, 2)
+then extract(year from sysdate) - (1900+substr(professor_ssn, 1, 2)) 
+--else    -> 현재년도 - 출생년도 - 1
+else extract(year from sysdate) - (1900 + substr(professor_ssn, 1, 2)) - 1
+end 나이
+from tb_professor
+order by 나이;
+
+
+--방법2.
+select professor_name 교수이름,
+            --버림((현재날짜 - 출생날짜 ) / 12)
+           trunc(months_between(sysdate, (19||substr(professor_ssn, 1, 6)))/12)나이
+from tb_professor
+order by 나이;
+
+
+--4. 교수들의 이름 중 성을 제외한 이름만 출력하는 SQL 문장을 작성하시오.
+--출력 헤더는 "이름" 이 찍히도록 한다. (성이 2자인 경우는 교수는 없다고 가정하시오)
+
+select substr(professor_name, 2) 이름
+from tb_professor
+order by professor_no;
+
+
+--5. 춘 기술대학교의 재수생 입학자를 구하려고 한다.
+--어떻게 찾아낼 것인가? 이때, 19살에 입학하면 재수를 하지 않은 것으로 간주한다.
+
+select student_no, student_name
+from tb_student
+where (extract(year from entrance_date) - extract(year from to_date((19 || substr(student_ssn, 1, 6)), 'yyyy/mm/dd' ))) > 19;
+
+--6. 2020년 크리스마스는 무슨 요일인가?
+
+SELECT to_char(to_date('2020/12/25'), 'dy')||'요일' "2020년 크리스마스"
+FROM dual;
+
+
+--7. TO_DATE('99/10/11','YY/MM/DD'), TO_DATE('49/10/11','YY/MM/DD') 은 각각 몇 년 몇 월 몇 일을 의미할까?
+--또 TO_DATE('99/10/11','RR/MM/DD'), TO_DATE('49/10/11','RR/MM/DD') 은 각각 몇 년 몇 월 몇 일을 의미할까?
+
+--yy는 현재년도 기준으로 현재세기(2000~2099)에서 추측한다.
+--rr은 현재년도 기준으로 앞 뒤 50년단위로(1950~2049) 추측한다.
+
+select to_char(TO_DATE('99/10/11','YY/MM/DD'), 'yyyy/mm/dd'),
+           to_char(TO_DATE('49/10/11','YY/MM/DD'), 'yyyy/mm/dd'),
+           to_char(TO_DATE('99/10/11','RR/MM/DD'), 'yyyy/mm/dd'), 
+           to_char(TO_DATE('49/10/11','RR/MM/DD'), 'yyyy/mm/dd')
+from dual;
+
+
+--8. 춘 기술대학교의 2000년도 이후 입학자들은 학번이 A로 시작하게 되어있다.
+--2000년도 이전 학번을 받은 학생들의 학번과 이름을 보여주는 SQL 문장을 작성하시오.
+
+select student_name, student_no
+from tb_student
+where student_no not like 'A%'
+order by student_name;
+
+
+--9. 학번이 A517178 인 한아름 학생의 학점 총 평점을 구하는 SQL 문을 작성하시오.
+--단, 이때 출력 화면의 헤더는 "평점" 이라고 찍히게 하고, 점수는 반올림하여 소수점 이하 한 자리까지만 표시한다.
+
+select round(avg(point), 1) 평점
+from tb_grade
+group by student_no
+having student_no = 'A517178';
+
+
+--10. 학과별 학생수를 구하여 "학과번호", "학생수(명)" 의 형태로 헤더를 만들어 결과값이 출력되도록 하시오.
+
+select department_no 학과번호, count(*) "학생수(명)"
+from tb_student
+group by department_no
+order by department_no;
+
+
+--11. 지도 교수를 배정받지 못한 학생의 수는 몇 명 정도 되는지 알아내는 SQL 문을 작성하시오.
+
+select count(*) - count(coach_professor_no)
+from tb_student;
+
+
+--12. 학번이 A112113인 김고운 학생의 년도 별 평점을 구하는 SQL 문을 작성하시오.
+--단, 이때 출력 화면의 헤더는 "년도", "년도 별 평점" 이라고 찍히게 하고,
+--점수는 반올림하여 소수점 이하 한 자리까지만 표시한다.
+
+select substr(term_no, 1, 4) 년도 , round(avg(point), 1) "년도 별 평점"
+from tb_grade
+where student_no = 'A112113'
+group by substr(term_no, 1, 4)
+order by 년도;
+
+
+--13. 학과 별 휴학생 수를 파악하고자 한다. 학과 번호와 휴학생 수를 표시하는 SQL 문장을 작성하시오.
+
+select *
+from tb_student;
+select department_no,
+count(student_name)
+from tb_student
+where absence_yn = 'Y'
+group by department_no
+order by 1;
+
+
+--14. 춘 대학교에 다니는 동명이인(同名異人) 학생들의 이름을 찾고자 한다. 어떤 SQL 문장을 사용하면 가능하겠는가?
+
+select student_name "동일이름", count(*) "동명인 수"
+from tb_student
+group by student_name
+having count(student_name) >= 2
+order by "동일이름";
+
+--15. 학번이 A112113 인 김고운 학생의 년도, 학기 별 평점과 년도 별 누적 평점 , 총 평점을 구하는 SQL 문을 작성하시오.
+--(단, 평점은 소수점 1자리까지만 반올림하여 표시한다.)
+
+select*
+from tb_grade;
+
+select decode(grouping(substr(TERM_NO,1,4)),0, substr(TERM_NO,1,4), '　') 년도,
+         decode(grouping(substr(TERM_NO,5,2)),0, substr(TERM_NO,5,2), '　') 학기,
+         round(avg(point),1) 평점
+from tb_grade
+where STUDENT_NO = 'A112113'
+group by rollup(substr(TERM_NO,1,4), 
+             substr(TERM_NO,5,2))
+order by 1,2;   
+
