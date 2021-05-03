75c7febcca8a2e698ec1cc09d966737ae7d5dc0a
NamYunji <dbs7wl7@gmail.com>
2021-05-03 16:52:55 +0900

    FoodServlet 생성

--- web_server_workspace/05_hellogit/src/com/kh/git/food/FoodServlet.java
+++ web_server_workspace/05_hellogit/src/com/kh/git/food/FoodServlet.java
@@ -7,16 +7,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class FoodServlet
- */
+
 @WebServlet("/food.do")
 public class FoodServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
