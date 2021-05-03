3f017aaaeaf64368da89fd59f8f7ac1789e3135d
NamYunji <dbs7wl7@gmail.com>
2021-05-03 16:38:31 +0900

    HelloGitServlet 추가!
    hellogitservlet을 추가한다.

--- web_server_workspace/05_hellogit/src/com/kh/git/HelloGitServlet.java
+++ web_server_workspace/05_hellogit/src/com/kh/git/HelloGitServlet.java
@@ -18,7 +18,7 @@ public class HelloGitServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stubdfdf
+		// TODO Auto-generated method stubdfdfㄹㅇㄷㄴ
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
